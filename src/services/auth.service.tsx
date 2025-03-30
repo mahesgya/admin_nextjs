9be38f9f12ddcfeaa967a7e1be43ecf4d757914b
unknown <anargya115@gmail.com>
2025-03-30 15:25:05 +0800

    first setup

---
+++ src/services/auth.service.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+interface LoginResponse { 
+    data: {
+        accessToken: string;
+        refreshToken: string;
+    };
+}
+
+const authService = {
+    loginAdmin: async (email:string, password:string, router: any) => {
+        try {
+            const response = await axios.post<LoginResponse>(`${BASE_URL}/api/admin/login`,{
+                email, 
+                password,
+            })
+
+            const {accessToken, refreshToken} = response.data.data;
+
+            Cookies.set("accessToken", accessToken, {secure: true, sameSite: "none", expires: 1});
+            Cookies.set("refreshToken", refreshToken, {secure: true, sameSite: "none", expires: 7});
+
+            router.push("/dashboard");
+            return response.data;
+
+        } catch (error) {
+           throw error;
+        }
+    }
+}
+
+export default authService;
