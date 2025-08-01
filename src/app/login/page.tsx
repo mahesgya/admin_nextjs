45fe5dfad25e22f400bc24972af45c23aff54607
mahesgya <anargya115@gmail.com>
2025-08-01 14:36:06 +0800

    edit: minor editing at login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,16 @@
 "use client";
+
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import Swal from "sweetalert2";
 
 import authService from "@/services/auth.service";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Eye, EyeOff } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
+import { AlertUtils } from "@/components/utils/alert.utils";
+
+import { Eye, EyeOff } from "lucide-react";
 import { PacmanLoader } from "react-spinners";
 
 const Login = () => {
@@ -29,21 +30,7 @@ const Login = () => {
       setPassword("");
       router.push("/dashboard");
     } catch (error : unknown) {
-      let errMsg = "Terjadi Kesalahan, silahkan coba lagi";
-      if(error instanceof AxiosError){
-        errMsg = error.response?.data?.errors;
-      }else if(error instanceof Error){
-        errMsg = error.message;
-      }else{
-        errMsg = "Terjadi Kesalahan, silahkan coba lagi";
-      }
-      Swal.fire({
-        icon: "error",
-        title: "Gagal Login",
-        text: error instanceof Error ? error.message : "Terjadi kesalahan, silahkan coba lagi.",
-        showConfirmButton : true,
-        showCloseButton : true,
-      })
+      AlertUtils.showError(error instanceof Error ? error.message : "Terjadi kesalahan, silahkan coba lagi.")
     }finally{
       setLoading(false);
     }
@@ -58,30 +45,30 @@ const Login = () => {
   }
 
   return (
-    <Card className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-tl from-[#64b5f6] via-[#b3e5fc] to-[#64b5f6]">
-      <CardContent className="max-w-md w-full mx-auto p-6 bg-white shadow-lg rounded-lg">
-        <img src="/images/LogoAkucuciin.png" alt="Logo AkuCuciin" className=" w-48 h-auto lg:w-[300px] mx-auto mb-2" />
+    <Card className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-tl from-[#64b5f6] via-[#b3e5fc] to-[#64b5f6] dark:bg-gradient-to-br dark:from-slate-900 dark:via-gray-900 dark:to-slate-900">
+      <CardContent className="max-w-md w-full mx-auto p-6 sm:p-8 bg-white/90 backdrop-blur-sm shadow-2xl rounded-2xl dark:bg-gray-800/80 dark:border dark:border-gray-700">
+        <img src="/images/LogoAkucuciin.png" alt="Logo AkuCuciin" className="w-48 h-auto lg:w-[200px] mx-auto mb-2" />
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <Label htmlFor="email" className="font-quick  block text-gray-700 font-semibold mb-2">
+            <Label htmlFor="email" className="font-quick  block text-gray-700 font-semibold mb-2 dark:text-white/90">
               Email
             </Label>
-            <Input required id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Masukkan email Anda" className="w-full p-3 border border-gray-300 rounded-lg " />
+            <Input required id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Masukkan email Anda" className="w-full p-3 border border-gray-300 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:placeholder-gray-400 dark:focus:ring-white/70" />
           </div>
           <div className="mb-4">
-            <Label htmlFor="password" className="block text-gray-700 font-semibold mb-2">
+            <Label htmlFor="password" className="block text-gray-700 font-semibold mb-2 dark:text-white/90">
               Password
             </Label>
             <div className="relative">
-              <Input required id="password" type={showPassword ? "text" : "password"} value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Masukkan password Anda" className="w-full p-3 border border-gray-300 rounded-lg" />
+              <Input required id="password" type={showPassword ? "text" : "password"} value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Masukkan password Anda" className="w-full p-3 border border-gray-300 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:placeholder-gray-400 dark:focus:ring-white/70" />
               <Button type="button" className="absolute right-0 top-0 bg-transparent text-gray-400" onClick={() => setShowPassword(!showPassword)}>{showPassword ? <EyeOff size={18} /> : <Eye size={18} />}</Button>
             </div>
           </div>
-          <Button type="submit" className="w-full mt-3 p-3 bg-[#64b5f6] text-white rounded-xl font-semibold shadow-md focus:outline-none focus:ring-2 hover:shadow-lg transition duration-300">
+          <Button type="submit" className="w-full mt-3 p-3 bg-[#64b5f6] text-white rounded-lg font-semibold shadow-md focus:outline-none focus:ring-2 hover:shadow-lg dark:focus:ring-offset-slate-800 transition duration-300">
             Login
           </Button>
         </form>
-        <p className="text-center text-gray-500 text-sm mt-4">© {new Date().getFullYear()} Admin Akucuciin</p>
+        <p className="text-center text-gray-500 text-sm mt-4 dark:text-white">© {new Date().getFullYear()} Admin Akucuciin</p>
       </CardContent>
     </Card>
   );
