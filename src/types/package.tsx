99c3402fccb5848c0617cca5f25933640a3f5d77
mahesgya <anargya115@gmail.com>
2025-08-01 21:43:34 +0800

    edit: modified package types

--- src/types/package.tsx
+++ src/types/package.tsx
@@ -2,23 +2,55 @@ export interface BasePackage {
   name: string;
   description: string;
   price_text: string;
+  features: string[];
 }
 
 export interface Package extends BasePackage {
   id: string;
 }
 
-export interface PackageForm extends BasePackage {
+export interface SinglePackage {
+  id: string;
+  name: string;
+  description: string;
+  price_text: string;
   features: string;
 }
 
-export interface PackageData extends PackageForm {
+export interface PackageData {
   id: string;
+  name: string;
+  email: string;
+  description: string;
+  telephone: string;
+  address: string;
+  city: string;
+  area: string;
+  maps_pinpoint: string;
+  latitude: string;
+  longitude: string;
+  created_at: string;
+  updated_at: string;
+  is_active: number;
+  is_open: number;
+  packages: Package[];
+}
+
+export interface PackageForm{
+  name: string;
+  description: string;
+  price_text: string;
+  features: string;
 }
 
 export interface PackageResponses {
   success: boolean;
   data: PackageData;
+}
+
+export interface PackageSingleResponse {
+  success: boolean;
+  data: SinglePackage;
 }
 
 export interface PostPackageResponse{ 
