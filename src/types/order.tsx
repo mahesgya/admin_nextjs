5130133980cc1f1551eab007d6ca9612db61dcea
mahesgya <anargya115@gmail.com>
2025-08-03 00:06:04 +0800

    feat: modified order types

--- src/types/order.tsx
+++ src/types/order.tsx
@@ -3,10 +3,19 @@ import { LaundryPartners } from "./laundry";
 import { DriverOrder } from "./driver";
 import { Package } from "./package";
 
+export interface Coupon {
+    name: string | null;
+    multiplier: number | null;
+    description: string | null;
+    min_weight: number | null;
+    max_discount: number | null;
+}
+
 export interface Orders {
     id: string;
     content: string;
     status: string;
+    cancel_reason: string | null;
     status_payment: string;
     maps_pinpoint: string;
     weight: string;
@@ -19,14 +28,19 @@ export interface Orders {
     rating: number;
     review: string | null;
     pickup_date: string;
+    payment_link: string | null;
     customer: Customer;
+    coupon : Coupon;
     laundry_partner: LaundryPartners;
     package : Package;
     driver : DriverOrder;
-
 }
 
 export interface OrderResponses {
     success: boolean;
     data: Orders[];
+}
+export interface OrderSingleResponse {
+    success: boolean;
+    data: Orders;
 }
